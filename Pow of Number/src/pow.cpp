db43dae06abdd9e2e1d49f33ad7c2ebff893f575
Farmyan <arminebaloyan.ab@gmail.com>
2025-04-22 18:40:23 +0400

    Update pow.cpp

--- Pow of Number/src/pow.cpp
+++ Pow of Number/src/pow.cpp
@@ -1,16 +1,19 @@
 double power(double x, int n) 
 {
-    if (x == 0 && n == 0) {
+    if (x == 0 && n == 0) 
+    {
         return 1; 
     }
     double number = x;
     int times = n;
-    if (n < 0) {
+    if (n < 0) 
+    {
         number = 1 / x;
         times = -n;
     }
     double result = 1;
-    for (int i = 0; i < times; i++) {
+    for (int i = 0; i < times; i++) 
+    {
         result *= number;
     }
     return result;
