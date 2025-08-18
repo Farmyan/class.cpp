e68de511b130858986076f012607cb634ba705cf
Farmyan <arminebaloyan.ab@gmail.com>
2025-08-18 13:20:52 +0400

    Update Organization.h

--- ClassHierarchy/header/Organization.h
+++ ClassHierarchy/header/Organization.h
@@ -1,15 +1,18 @@
 // header/Organization.h
 #pragma once
 #include <string>
-class Organization {
-protected:
-    std::string name;
-    std::string country;
-    std::string industry;
-public:
-    Organization(std::string n, std::string c, std::string i)
-        : name(n), country(c), industry(i) {}
-    
-    virtual std::string getDescription() const = 0;
-    virtual ~Organization() = default;
-};
+
+class Organization 
+    {
+        protected:
+            std::string name;
+            std::string country;
+            std::string industry;
+        public:
+            Organization(std::string n, std::string c, std::string i) : name(n), country(c), industry(i) {}
+            const std::string& getName() const { return name; }
+            const std::string& getCountry() const { return country; }
+            const std::string& getIndustry() const { return industry; }
+            virtual std::string getDescription() const = 0;
+            virtual ~Organization() {}
+    };
