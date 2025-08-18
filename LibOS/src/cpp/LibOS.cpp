2a4a0e72becace6c756f4f632eb1a3036d27c0e5
Farmyan <arminebaloyan.ab@gmail.com>
2025-08-18 20:16:50 +0400

    Update LibOS.cpp

--- LibOS/src/cpp/LibOS.cpp
+++ LibOS/src/cpp/LibOS.cpp
@@ -1,41 +1,48 @@
 #include "LibOS.h"
 #include <sys/stat.h>
 
-void File::open(const std::string& path, int flags, int mode) {
-    fd = ::open(path.c_str(), flags, mode);
-    if (fd < 0) {
-        throw OpenFileException();
+void File::open(const std::string& path, int flags, int mode) 
+    {
+        fd = ::open(path.c_str(), flags, mode);
+        if (fd < 0) 
+            {
+                throw OpenFileException();
+            }
+        filename = path;
     }
-    filename = path;
-}
 
-int File::write(const std::string& data) {
-    if (fd < 0) throw WriteFileException();
-    return ::write(fd, data.c_str(), data.size());
-}
-
-int File::read(char* buffer, int size) {
-    if (fd < 0) throw ReadFileException();
-    return ::read(fd, buffer, size);
-}
-
-void File::close() {
-    if (fd >= 0) {
-        ::close(fd);
-        fd = -1;
+int File::write(const std::string& data) 
+    {
+        if (fd < 0) throw WriteFileException();
+        return ::write(fd, data.c_str(), data.size());
     }
-}
 
-// Directory methods using system calls
-void Directory::create(const std::string& path) {
-    if (::mkdir(path.c_str(), 0755) < 0) throw CreateDirException();
-}
+int File::read(char* buffer, int size) 
+    {
+        if (fd < 0) throw ReadFileException();
+        return ::read(fd, buffer, size);
+    }
 
-void Directory::remove(const std::string& path) {
-    if (::rmdir(path.c_str()) < 0) throw RemoveDirException();
-}
+void File::close() 
+    {
+        if (fd >= 0) 
+            {
+                ::close(fd);
+                fd = -1;
+            }
+    }
 
-void Directory::list(const std::string&) {
-    // Stub: you can implement opendir/readdir if needed
-    throw ListDirException();
-}
+void Directory::create(const std::string& path) 
+    {
+        if (::mkdir(path.c_str(), 0755) < 0) throw CreateDirException();
+    }
+
+void Directory::remove(const std::string& path) 
+    {
+        if (::rmdir(path.c_str()) < 0) throw RemoveDirException();
+    }
+
+void Directory::list(const std::string&) 
+    {
+        throw ListDirException();
+    }
