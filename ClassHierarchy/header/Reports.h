32dee1b4d7554e5e69d5ff5a89c0da48a709492d
Farmyan <arminebaloyan.ab@gmail.com>
2025-09-07 16:35:32 +0400

    Update Reports.h

--- ClassHierarchy/header/Reports.h
+++ ClassHierarchy/header/Reports.h
@@ -1,40 +1,24 @@
 #pragma once
-#include <string>
-#include <iostream>
 #include "Registrar.h"
+#include <iostream>
+#include <string>
 
 class Report 
     {
         public:
-            virtual ~Report() {}
-            virtual void generate(const Registrar& r) const = 0;
-    };
-
-class CSVDetailReport : public Report 
-    {
-        public:
-            void generate(const Registrar& r) const override 
+            void generate(Registrar& reg) 
                 {
-                    std::cout << "Type,Name,Country,Industry,Description\n";
-                    for (int i = 0; i < r.size(); i++) 
+                    for (int i = 0; i < reg.size(); i++) 
                         {
-                            Organization* o = r.get(i);
-                            std::string desc = o->getDescription();
-                            std::string type = desc.substr(0, desc.find(':'));
-                            std::cout << type << "," << o->getName() << "," << o->getCountry() << "," << o->getIndustry() << "," << desc << "\n";
+                            Organization* o = reg.get(i);
+                            if (o) 
+                                {
+                                    std::cout << "Name: " << o->getName() << "\n";
+                                    std::cout << "Country: " << o->getCountry() << "\n";
+                                    std::cout << "Industry: " << o->getIndustry() << "\n";
+                                    std::cout << "Description: " << o->getDescription() << "\n\n";
+                                }
                         }
                 }
-    };
-
-class MarkdownDetailReport : public Report 
-    {
-        public:
-            void generate(const Registrar& r) const override 
-                {
-                    for (int i = 0; i < r.size(); i++) 
-                        {
-                            Organization* o = r.get(i);
-                            std::cout << "- " << o->getDescription() << "\n";
-                        }
-                }
+            virtual ~Report() {} 
     };
