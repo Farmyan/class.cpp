401e3b72bfd2eeea5d38e295f7b03f039377833f
Farmyan <arminebaloyan.ab@gmail.com>
2025-09-07 16:35:15 +0400

    Update Registrar.h

--- ClassHierarchy/header/Registrar.h
+++ ClassHierarchy/header/Registrar.h
@@ -1,57 +1,40 @@
 #pragma once
-#include <string>
-#include <iostream>
 #include "Organization.h"
 
 class Registrar 
     {
-        Organization** db;    
-        int capacity;
+        Organization* orgs[100]; 
         int count;
-
-        void grow() 
-            {
-                int newCap = capacity * 2;
-                Organization** newDb = new Organization*[newCap];
-                for (int i = 0; i < count; i++)
-                   {
-                        newDb[i] = db[i];
-                   }
-                delete[] db;
-                db = newDb;
-                capacity = newCap;
-            }
         public:
-            Registrar() : capacity(10), count(0) 
-                {
-                    db = new Organization*[capacity];
-                }
-            ~Registrar() 
-                {
-                    for (int i = 0; i < count; i++)
-                        delete db[i];
-                    delete[] db;
-                }
+            Registrar() : count(0) {}
             bool registerOrg(Organization* org) 
                 {
+                    if (count >= 100) 
+                        {
+                            return false;
+                        }
                     for (int i = 0; i < count; i++) 
                         {
-                            if (db[i]->getName() == org->getName() && db[i]->getCountry() == org->getCountry()) 
+                            if (orgs[i]->getName() == org->getName()) 
                                 {
+                                    delete org; 
                                     return false;
                                 }
                         }
-                    if (count >= capacity) grow();
-                    db[count++] = org;
+
+                    orgs[count++] = org;
                     return true;
                 }
             int size() const 
                 { 
                     return count; 
                 }
-            Organization* get(int index) const 
+            Organization* get(int index) 
                 {
-                    if (index < 0 || index >= count) return 0;
-                    return db[index];
-                }
+                    if (index < 0 || index >= count) 
+                        {
+                            return nullptr;
+                        }
+                    return orgs[index];
+                }   
     };
